b8297014d1c34b1c9871b40ce38713fbeebd0c04
Luigi Feriozzi <luigiferiozzi@gmail.com>
2026-03-24 17:55:43 +0100

    test cd

--- client/src/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Move, Color as PieceColor, PieceSymbol, Square } from "chess.js";
-
-export type SquareState = {
-  square: Square;
-  type: PieceSymbol;
-  color: PieceColor;
-};
-export type ChessJsBoard = (SquareState | null)[][];
-export type Strategy = (moves: Move[]) => Move;
--- client/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export function randomChoice<T>(choices: T[]): T {
-  if (choices.length <= 0)
-    throw "Can't make a random choice because there is no choice";
-  return choices[Math.floor(Math.random() * choices.length)];
-}
